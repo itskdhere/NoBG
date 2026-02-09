42b2d69582c802a63d2308bff445ad352c933d92
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2026-02-10 01:50:09 +0530

    add vercel analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Providers } from "@/components/providers";
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
